e7b9645eead4440d99f727648588751b3c3cf17b
Himangsu <himangsusarkar1@gmail.com>
2022-01-09 20:34:22 +0800

    export import user module

---
+++ Lession1/index.js
@@ -0,0 +1,7 @@
+'use strict'
+// const s1 = require('./students');
+const{getName}=require('./students')
+
+console.log(getName());
+// console.log(s1.getAge());
+// console.log(s1.CGP);
---
+++ Lession1/students.js
@@ -0,0 +1,19 @@
+ exports.getName = () => {
+  return "Himangsu";
+}
+// const getName = () => {
+//   return "Himangsu"
+// }
+const getAge = () => {
+  return "24";
+}
+const CGP=3.65
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = CGP;
+// module.exports = {
+//   getName,
+//   getAge,
+//   CGP
+// }
